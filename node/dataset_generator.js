4bf154c78c8b0216617fa082814357b2788ec5ec
Henning-Kubatzsch <henningkb@outlook.com>
2025-02-25 08:13:14 +0100

    added confusion matrix and 3rd dimension

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -7,8 +7,9 @@ const canvas = createCanvas(400, 400);
 const ctx = canvas.getContext("2d");
 
 const fs = require("fs");
+const geometry = require("../common/geometry.js");
 
-console.log("we are in");
+//console.log("we are in");
 
 // This section helps to install the package
 if (fs.existsSync(constants.DATASET_DIR)) {
@@ -33,20 +34,22 @@ fileNames.forEach((fn) => {
    const content = fs.readFileSync(constants.RAW_DIR + "/" + fn);
    const { session, student, drawings } = JSON.parse(content);
    for (let label in drawings) {
-      samples.push({
-         id,
-         label,
-         student_name: student,
-         student_id: session,
-      });
-
-      const paths = drawings[label];
-      fs.writeFileSync(
-         constants.JSON_DIR + "/" + id + ".json",
-         JSON.stringify(paths)
-      );
-
-      generateImageFile(constants.IMG_DIR + "/" + id + ".png", paths);
+      if(!utils.flaggedSamples.includes(id)){
+         samples.push({
+            id,
+            label,
+            student_name: student,
+            student_id: session,
+         });
+   
+         const paths = drawings[label];
+         fs.writeFileSync(
+            constants.JSON_DIR + "/" + id + ".json",
+            JSON.stringify(paths)
+         );   
+         generateImageFile(constants.IMG_DIR + "/" + id + ".png", paths);
+      }
+      
 
       utils.printProgress(id, fileNames.length * 8);
       id++;
@@ -66,6 +69,13 @@ function generateImageFile(outFile, paths) {
    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
    draw.paths(ctx, paths);
+
+   const{vertices, hull} = geometry.minimumBoundingBox({
+      points: paths.flat()
+   });
+   draw.path(ctx, [...vertices, vertices[0]], "red");
+   draw.path(ctx, [...hull, hull[0]], "blue");
+
 
    const buffer = canvas.toBuffer("image/png");
    fs.writeFileSync(outFile, buffer);
--- node/run_evaluation.js
+++ node/run_evaluation.js
@@ -13,7 +13,7 @@ const { samples: trainingSamples } = JSON.parse(
 
 const kNN = new KNN(trainingSamples);
 
-const { samples: testingSamples } = JSON.parse(
+const {samples : testingSamples } = JSON.parse(
    fs.readFileSync(constants.TESTING)
 );
 
@@ -45,6 +45,7 @@ const ctx = canvas.getContext("2d");
 for (let x = 0; x < canvas.width; x++) {
    for (let y = 0; y < canvas.height; y++) {
       const point = [x / canvas.width, 1 - y / canvas.height];
+      point.push(0.2);
       const { label } = kNN.predict(point);
       const color = utils.styles[label].color;
       ctx.fillStyle = color;
